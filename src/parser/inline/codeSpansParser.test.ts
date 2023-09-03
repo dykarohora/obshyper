e8402239e55204b00516afa38883a606aba45c57
d_yama <7d.yama@gmail.com>
2023-09-04 00:45:43 +0900

    コードスパンパーサを実装した

--- src/parser/inline/codeSpansParser.test.ts
+++ src/parser/inline/codeSpansParser.test.ts
@@ -1,28 +1,30 @@
 import { codeSpansParser } from './codeSpansParser.js'
-import { type ParserOutput } from '@dykarohora/funser'
+import { lookBehind, noCharOf, not, type ParserOutput, pipe, seq, string } from '@dykarohora/funser'
 import { type CodeSpans } from '../../types/index.js'
 
 describe('CodeSpansParser', () => {
 	it.each([
-		['`foo`', 'foo'],
-		['`` foo ` bar ``', 'foo ` bar'],
-		['` `` `', '``'],
-		['`  ``  `', '``'],
-		['` a`', ' a'],
-		['`　a　`', '　a　'],
-		['`  `', '  '],
-		['`　`', '　'],
-		['``\nfoo\nbar  \nbaz``', 'foo bar   baz'],
-		['``\nfoo \n``', 'foo '],
-		['`foo   bar \nbaz`', 'foo   bar  baz'],
-		['`foo\\`bar`', 'foo\\'],
-		['` foo `` bar `', 'foo `` bar'],
-	])('入力が「%s」のとき、パースに成功し結果として「%s」を取得できる', (input, expected) => {
+		['`a`', 'a', 3],
+		['`foo`', 'foo', 5],
+		['``foo``', 'foo', 7],
+		['`` foo ` bar ``', 'foo ` bar', 15],
+		['` `` `', '``', 6],
+		['`  ``  `', ' `` ', 8],
+		['` a`', ' a', 4],
+		['`　a　`', '　a　', 5],
+		['`  `', '  ', 4],
+		['`　`', '　', 3],
+		['``\nfoo\nbar  \nbaz\n``', 'foo bar   baz', 19],
+		['``\nfoo \n``', 'foo ', 10],
+		['`foo   bar \nbaz`', 'foo   bar  baz', 16],
+		['`foo\\`bar`', 'foo\\', 6],
+		['` foo `` bar `', 'foo `` bar', 14],
+	])('入力が「%s」のとき、パースに成功し結果として「%s」を取得できる', (input, expected, lastIndex) => {
 		const output = codeSpansParser({ input })
 		const expectedOutput: ParserOutput<CodeSpans> = {
 			type: 'Success',
 			value: { type: 'codeSpans', value: expected },
-			state: { input, position: input.length }
+			state: { input, position: lastIndex }
 		}
 
 		expect(output).toEqual(expectedOutput)
@@ -37,11 +39,3 @@ describe('CodeSpansParser', () => {
 		expect(output.type).toEqual('Failure')
 	})
 })
-
-// TODO 以下のパターンのテストケースが必要
-// 「*foo`*`」
-// 「[not a `link](/foo`)」
-// 「`<a href="`">`」
-// 「`<http://foo.bar.`baz>`」
-// 「<http://foo.bar.`baz>`」
-// https://spec.commonmark.org/0.30/#backtick-string
--- src/parser/inline/codeSpansParser.ts
+++ src/parser/inline/codeSpansParser.ts
@@ -1,7 +1,68 @@
 import type { Parser } from '@dykarohora/funser/types'
 import type { CodeSpans } from '../../types/index.js'
+import {
+	anyChar,
+	anyCharOf,
+	map,
+	noCharOf,
+	not,
+	pipe,
+	repeat,
+	repeatTill,
+	seq,
+	string
+} from '@dykarohora/funser'
+
+const backQuotes = pipe(
+	anyCharOf('`'),
+	repeat({ min: 1 })
+)
 
 export const codeSpansParser: Parser<CodeSpans> =
 	({ input, position = 0 }) => {
-		throw new Error('not implemented')
+		const output = backQuotes({ input, position })
+
+		if (output.type === 'Failure') {
+			return output
+		}
+
+		const prefix = output.value.join('')
+
+		const postfixParser =
+			pipe(
+				noCharOf('`'),
+				seq(string(prefix)),
+				seq(not(string('`'))),
+				map(([[tail, _], __]) => tail)
+			)
+
+		const parser = pipe(
+			anyChar,
+			repeatTill(postfixParser),
+		)
+
+		const output2 = parser(output.state)
+
+		if (output2.type === 'Failure') {
+			return {
+				...output2,
+				state: { input, position }
+			}
+		}
+
+		let content = output2.value.join('').replace(/\n/g, ' ')
+		const hasNonSpaceChars = /[^ ]/.test(content)
+		const hasSpaceCharsOnBothEnds = content.startsWith(' ') && content.endsWith(' ')
+		if (hasNonSpaceChars && hasSpaceCharsOnBothEnds) {
+			content = content.substring(1, content.length - 1)
+		}
+
+		return {
+			type: 'Success',
+			value: {
+				type: 'codeSpans',
+				value: content
+			},
+			state: output2.state
+		}
 	}
