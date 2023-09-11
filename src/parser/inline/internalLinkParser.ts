c1839172ceb1d035dd12e244e5b2eb74c7331510
d_yama <7d.yama@gmail.com>
2023-09-11 22:36:45 +0900

    internalLinkパーサをリファクタした

--- src/parser/inline/internalLinkParser.ts
+++ src/parser/inline/internalLinkParser.ts
@@ -1,41 +1,39 @@
 import type { Parser } from '@dykarohora/funser/types'
 import type { InternalLink } from '../../types/index.js'
-import { anyChar, map, noCharOf, option, orParser, pipe, repeatTill, seq, seqParser, string } from '@dykarohora/funser'
+import { anyChar, map, noCharOf, option, orParser, pipe, repeatTill, seqParser, string } from '@dykarohora/funser'
+
+const path =
+	pipe(
+		noCharOf('#^[]/:\\'),
+		repeatTill(orParser(string('|'), string(']]')), { includeTillResult: false, consumption: false }),
+		map(chars => chars.join(''))
+	)
+
+const displayText =
+	pipe(
+		seqParser(
+			string('|'),
+			pipe(
+				anyChar,
+				repeatTill(string(']]'), { includeTillResult: false, consumption: false }),
+			)
+		),
+		map(([, chars]) => chars.join(''))
+	)
 
 export const internalLinkParser: Parser<InternalLink> =
 	pipe(
 		seqParser(
 			option(string('!')),
 			string('[['),
-			pipe(
-				noCharOf('#^[]/:\\'),
-				repeatTill(orParser(string('|'), string(']]')), { includeTillResult: false, consumption: false }),
-				map(chars => chars.join('')),
-				seq(
-					pipe(
-						string('|'),
-						seq(
-							pipe(
-								anyChar,
-								repeatTill(string(']]'), { includeTillResult: false, consumption: false }),
-								map(chars => chars.join(''))
-							)
-						),
-						option
-					)
-				)
-			),
+			path,
+			option(displayText),
 			string(']]')
 		),
-		map(([embedding, , [path, text]]) => {
-
-			const internalLink: InternalLink = {
-				type: 'internalLink',
-				path,
-				text: text.type === 'Some' ? text.value[1] : path,
-				embedding: embedding.type === 'Some'
-			}
-
-			return internalLink
-		})
+		map(([embedding, _, path, text]) => ({
+			type: 'internalLink',
+			path,
+			text: text.type === 'Some' ? text.value : path,
+			embedding: embedding.type === 'Some'
+		}))
 	)
